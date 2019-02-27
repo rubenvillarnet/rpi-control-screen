3264a6b77c9e3bf225ba94ccb29e8a59a73426e4
Rubén Villar Grela <ruben@rubenvillar.net>
2019-02-27 20:11:49 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const internalIp = require('internal-ip');
+var exec = require('child_process').exec;
+
+
+function shutdown(callback) {
+  exec('shutdown now', function (error, stdout, stderr) { callback(stdout); });
+}
+
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  let data = {
+    ip: internalIp.v4.sync()
+  }
+
+  res.render('index', data);
+});
+
+router.put("/shutdown", (req, res, next) => {
+  shutdown(function (output) {
+    console.log(output);
+  });
+})
+
+module.exports = router;
