aa317ce721ac622fb4bea1bb276429ec628ee34d
Rubén Villar Grela <ruben@rubenvillar.net>
2019-02-28 13:37:36 +0100

    added arduino code

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+var five = require("johnny-five");
+var board = new five.Board();
 const internalIp = require('internal-ip');
 var exec = require('child_process').exec;
 
@@ -7,7 +9,15 @@ function shutdown(callback) {
   exec('sudo /sbin/shutdown now', function (msg) { console.log(msg) });
 }
 
-//require('child_process').exec('sudo /sbin/shutdown -r now', function (msg) { console.log(msg) });
+board.on("ready", () => {
+
+  servo = new five.Servo({
+    pin: 9,
+    center: true
+  })
+});
+
+
 
 
 /* GET home page */
@@ -25,6 +35,80 @@ router.post("/shutdown", (req, res, next) => {
   shutdown(function (output) {
     console.log(output);
   })
+})
+
+
+router.get('/arduino', (req, res, next) => {
+
+  res.render('arduino');
+});
+
+router.post('/arduino', (req, res, next) => {
+
+
+
+
+
+  switch (req.body.mov) {
+
+    case 38:
+      console.log("Hacia adelante")
+      var configs = five.Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1;
+      //Motor B es el derecho
+      var motorA = new five.Motor(configs.A);
+      var motorB = new five.Motor(configs.B);
+      motorB.forward(255)
+      motorA.forward(255)
+
+      res.json({ distancia: "distancia" })
+      break
+    case 40:
+
+      console.log("Hacia detras")
+      var configs = five.Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1;
+
+      var motorA = new five.Motor(configs.A);
+      var motorB = new five.Motor(configs.B);
+
+      motorA.reverse(255)
+      motorB.reverse(255)
+
+      res.json({ distancia: "distancia" })
+
+      break
+    case 37:
+
+      var configs = five.Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1;
+
+      var motorB = new five.Motor(configs.B);
+
+      motorB.forward(255)
+      res.json({ distancia: "distancia" })
+
+
+      break;
+    case 39:
+      var configs = five.Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1;
+
+      var motorA = new five.Motor(configs.A);
+
+      motorA.forward(255)
+      res.json({ distancia: "distancia" })
+
+      break;
+    case "stop":
+      var configs = five.Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1;
+
+      var motorA = new five.Motor(configs.A);
+      var motorB = new five.Motor(configs.B);
+
+      motorA.stop()
+      motorB.stop()
+      res.json({ distancia: "distancia" })
+      break
+    default:
+      break
+  }
 })
 
 module.exports = router;
